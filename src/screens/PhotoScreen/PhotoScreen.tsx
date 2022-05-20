312f8a348b49c897661b6b81f12a42165cd354b2
Abdur Rakib <abdurrakib961@gmail.com>
2022-05-20 11:46:46 +0600

    feat: implement sharing photo

--- src/screens/PhotoScreen/PhotoScreen.tsx
+++ src/screens/PhotoScreen/PhotoScreen.tsx
@@ -1,12 +1,32 @@
 import {Image, View, Text, Pressable} from 'react-native';
 import React from 'react';
+import Share from 'react-native-share';
 import {commonStyles} from '../../styles/commonStyles';
 import styles from './styles';
-import {spacing} from '../../theme';
 
 const PhotoScreen = ({route}) => {
   const {item} = route.params;
 
+  // handle share image
+  const handleShareImage = async () => {
+    const options = {
+      title: item.description,
+      url: item.urls.regular,
+      message: '',
+    };
+    try {
+      const shareResponse = await Share.open(options);
+      console.log(
+        '🚀 ~ file: PhotoScreen.tsx ~ line 23 ~ handleShareImage ~ shareResponse',
+        shareResponse,
+      );
+    } catch (err) {
+      console.log(
+        '🚀 ~ file: PhotoScreen.tsx ~ line 28 ~ handleShareImage ~ err',
+        err,
+      );
+    }
+  };
   return (
     <View style={commonStyles.container}>
       <View style={styles.imageContainer}>
@@ -22,7 +42,7 @@ const PhotoScreen = ({route}) => {
         <Pressable style={styles.actionButton}>
           <Text style={styles.actionText}>Save</Text>
         </Pressable>
-        <Pressable style={styles.actionButton}>
+        <Pressable onPress={handleShareImage} style={styles.actionButton}>
           <Text style={styles.actionText}>Share</Text>
         </Pressable>
       </View>
