ee6d0737c5945b267946ae0c640b736fe194c2d7
Abdur Rakib <abdurrakib961@gmail.com>
2022-05-20 16:24:43 +0600

    feat(splash_screen): chnage splash screen for ios devices

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StatusBar} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import SplashScreen from 'react-native-splash-screen';
 import GalleryStackNavigator from './src/navigation/GalleryStackNavigator';
 import {colors} from './src/theme';
 
-const App = () => {
+const App: React.FC = () => {
+  // hide splash screen
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <SafeAreaProvider>
       <StatusBar backgroundColor={colors.primary} barStyle="dark-content" />
