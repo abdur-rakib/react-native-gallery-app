4e0ad1d56e0208e768906e2d240093720cc25803
Abdur Rakib <abdurrakib961@gmail.com>
2022-05-27 18:04:53 +0600

    feat: get per page from variable

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,9 @@
-import {baseUrl, CLIENT_KEY} from '../config';
+import {baseUrl, CLIENT_KEY, PER_PAGE} from '../config';
 
 // get images from unsplash
 const fetchImages = async ({pageParam = 1}) => {
   const response = await fetch(
-    `${baseUrl}?client_id=${CLIENT_KEY}&page=${pageParam}&per_page=30`,
+    `${baseUrl}?client_id=${CLIENT_KEY}&page=${pageParam}&per_page=${PER_PAGE}`,
   );
   const results = await response.json();
   return {results, nextPage: pageParam + 1};
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,10 @@
 // client key
 const CLIENT_KEY = 'XdymHxISP3LPo2P0RGC6UJVNPqDy2q8UbLOI3vwaRyg';
 
+// per page data
+const PER_PAGE = 20;
+
 // base url
 const baseUrl = 'https://api.unsplash.com/photos/';
 
-export {baseUrl, CLIENT_KEY};
+export {baseUrl, CLIENT_KEY, PER_PAGE};
