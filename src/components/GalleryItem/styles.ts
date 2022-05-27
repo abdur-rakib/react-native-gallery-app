81a10d61a29b9194d489f4e4a5fe6f0429090968
Abdur Rakib <abdurrakib961@gmail.com>
2022-05-27 18:15:20 +0600

    fix: fix gallery images align

--- src/components/GalleryItem/styles.ts
+++ src/components/GalleryItem/styles.ts
@@ -3,12 +3,12 @@ import {StyleSheet} from 'react-native';
 const styles = StyleSheet.create({
   container: {
     height: 120,
-    width: '33%',
-    alignItems: 'center',
+    width: '33.33%',
   },
   thumbnail: {
     height: '96%',
     width: '96%',
+    alignSelf: 'center',
   },
 });
 
